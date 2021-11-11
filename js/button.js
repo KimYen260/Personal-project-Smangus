762a774fefd23efdf1ca42b209c05ffc2f104a05
Kim Yen <yen973@gmail.com>
2021-11-11 23:19:22 +0800

    commit一版

--- js/button.js
+++ js/button.js
@@ -1,8 +1,16 @@
 
-  var hamburger = document.querySelector(".hamburger");
-  // On click
-  hamburger.addEventListener("click", function() {
-    // Toggle class "is-active"
-    hamburger.classList.toggle("is-active");
-    // Do something else, like open/close menu
+  // var hamburger = document.querySelector(".hamburger");
+  // // On click
+  //     hamburger.addEventListener("click", function() {
+  //   // Toggle class "is-active"
+  //     hamburger.classList.toggle("is-active");
+  //   // Do something else, like open/close menu
+  // });
+
+  $(function(){
+    $("button.hamburger").on("click", function(){
+      $("button.hamburger").toggleClass("is-active");
+      $("nav.main_nav").toggle(1500);
+      // console.log($("button.hamburger"));
+    });
   });
